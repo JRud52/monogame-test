b3fa92af703768b20087b24528ff2b01ac77918c
Justin Rhude <jrud52@thinkarch.hitronhub.home>
2017-02-09 17:55:17 -0500

    first commit

---
+++ GameTest/Game1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GameTest
+{
+	public class Game1 : Game
+	{
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+		Player player;
+
+		public Game1()
+		{
+			graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+
+			IsFixedTimeStep = true;
+			graphics.SynchronizeWithVerticalRetrace = true;
+			TargetElapsedTime = new System.TimeSpan(0, 0, 0, 0, 33); // 33ms = 30fps
+		}
+
+		// initialize non-graphic content here
+		protected override void Initialize()
+		{
+			base.Initialize();
+		}
+
+		// load all the content
+		protected override void LoadContent()
+		{
+			// Create a new SpriteBatch, which can be used to draw textures.
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+			//texture = Content.Load<Texture2D>("sprites/sprite");
+			player = new Player(GraphicsDevice, new Vector2(100,100), Color.Red);
+
+		}
+
+		//unload all the content
+		protected override void UnloadContent()
+		{
+			Content.Unload();
+			base.UnloadContent();
+		}
+
+		//when window has focus
+		protected override void OnActivated(object sender, EventArgs args)
+		{
+			Window.Title = "Active Application";
+			base.OnActivated(sender, args);
+		}
+
+		// when window does not have focus
+		protected override void OnDeactivated(object sender, EventArgs args)
+		{
+			Window.Title = "InActive Application";
+			base.OnDeactivated(sender, args);
+		}
+
+		// update loop
+		protected override void Update(GameTime gameTime)
+		{
+			if (IsActive)
+			{
+				
+				
+				if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+					Exit();
+				
+
+				base.Update(gameTime);
+			}
+		}
+
+		// draws the graphics to the window
+		protected override void Draw(GameTime gameTime)
+		{
+			graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			spriteBatch.Begin();
+			spriteBatch.Draw(player.texture, player.pos);
+			spriteBatch.End();
+
+			base.Draw(gameTime);
+		}
+	}
+}
---
+++ GameTest/Program.cs
@@ -0,0 +1,26 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+#endregion
+
+namespace GameTest
+{
+	static class Program
+	{
+		private static Game1 game;
+
+		internal static void RunGame()
+		{
+			game = new Game1();
+			game.Run();
+		}
+
+		[STAThread]
+
+		static void Main(string[] args)
+		{
+			RunGame();
+		}
+	}
+}
